39efd6c9dea40f15d57e344a4f4d398e483a9179
Mathew Akre <mwakre29@gmail.com>
2019-10-17 13:39:40 -0700

    testing practice

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -7,7 +7,34 @@ namespace ToDoList.Tests
   public class ItemTest
   {
 
-    // Test methods will go here.
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Walk the dog.";
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+      public void SetDescription_SetDescription_String()
+      {
+        string description = "Walk the dog.";
+        Item newItem = new Item(description);
+
+        string updatedDescription = "Do the dishes";
+        newItem.Description = updatedDescription;
+        string result = newItem.Description;
+
+        Assert.AreEqual(updatedDescription, result);
+      }
 
   }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,9 +1,18 @@
+using System.Collections.Generic;
+
 namespace ToDoList.Models
 {
   public class Item
   {
+    public string Description { get; set; }
 
-    // properties, methods, etc. will go here.
+    private static ToDoList<Item> _instances = new List<Item> {};
+
+    public Item(string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
 
   }
 }
