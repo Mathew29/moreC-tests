cb8728089efd403ae0b3c844b949a100c4c6b7aa
Mathew Akre <mwakre29@gmail.com>
2019-10-31 12:07:51 -0700

    add connection to db

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -48,4 +48,9 @@ namespace ToDoList
       });
     }
   }
+  
+  public static class DBConfiguration
+    {
+        public static string ConnectionString = "server=localhost;user id=root;password=test; port=3306;database=to_do_list;"
+    }
 }
